8569f55246d66f4818d0d05c5c41fc8da0dfb09b
ibrahim saad <ibrahim.saad.dev@gmail.com>
2026-06-05 18:55:50 +0300

    Remove app.run for gunicorn

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 from flask import Flask
-import logging
+
 app = Flask(__name__)
-logging.basicConfig(level=logging.INFO)
 
 @app.route("/")
 def home():
-    app.logger.info("Home page visited")
     with open("index.html", "r") as file:
         return file.read()
-
-@app.route("/health")
-def health():
-    return "OK"
-
